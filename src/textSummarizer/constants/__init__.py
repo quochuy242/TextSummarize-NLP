e7cf74d4b5b4822e7fcc68c0be7d6eb887174764
Your Name <you@example.com>
2024-03-13 22:57:15 +0700

    run succesfully all block code in data_ingestion.ipynb

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
 
-CONFIG_FILE_PATH = Path("d:/Python Project/TextSummarize-NLP/config/config.yaml")
-PARAMS_FILE_PATH = Path("d:/Python Project/TextSummarize-NLP/params.yaml")
+filepath_project = os.getcwd()
+CONFIG_FILE_PATH = Path(filepath_project + "/config/config.yaml")
+PARAMS_FILE_PATH = Path(filepath_project + "/params.yaml")
+
+print(CONFIG_FILE_PATH, "\n", PARAMS_FILE_PATH)
