7dc7c9abc87010774b701f3c78429e5415393772
Your Name <you@example.com>
2024-03-18 20:23:45 +0700

    update data validation

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-PROJECT_PATH = Path(os.getcwd())
+PROJECT_PATH = Path("D:/Python Project/TextSummarize-NLP")
 CONFIG_FILE_PATH = Path(f"{PROJECT_PATH}/config/config.yaml")
 PARAMS_FILE_PATH = Path(f"{PROJECT_PATH}/params.yaml")
 
---
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,13 @@
+from src.textSummarizer.components.data_validation import DataValidation
+from src.textSummarizer.config.configuration import ConfigurationManager
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
